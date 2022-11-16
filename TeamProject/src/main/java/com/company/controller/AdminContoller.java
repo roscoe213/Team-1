f51b7cb24e93e73bd45af9a56978238abd6d5ad9
jun_0 <jun_0@DESKTOP-RV9FDFT>
2022-11-16 19:31:07 +0900

    admin 함수추가

--- TeamProject/src/main/java/com/company/controller/AdminContoller.java
+++ TeamProject/src/main/java/com/company/controller/AdminContoller.java
@@ -1,6 +1,7 @@
 package com.company.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.company.admin.command.AdminCriteria;
 import com.company.admin.command.AdminPageVO;
@@ -47,6 +49,22 @@ public class AdminContoller {
 		service.adRegister(vo);
 		
 		return "redirect:"+referer;
+	}
+	
+	@RequestMapping("/adMap")
+	public String adMap(@RequestParam("num") int num,
+			Model model, @ModelAttribute("cri") Criteria cri){       //8.페이징에서 추가할 내용이 존재함."
+		System.out.println("===컨트롤러===");
+		System.out.println(num);
+		
+		
+		//num(bno)을 가지고 있는 데이터(row)를 불러오는 메서드
+		AdminVO vo = service.adContent(num);
+		model.addAttribute("link", vo);	
+		
+		
+		
+		return "admin/adMap";
 	}
 	
 }
