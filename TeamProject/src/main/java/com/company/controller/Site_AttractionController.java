611da031d8191136b0b9e63a3b904539ddaf3adb
akxlf <akxlf@JIN>
2022-12-01 16:04:43 +0900

    댓글 기능 수정 및 페이징 처리 90%

--- TeamProject/src/main/java/com/company/controller/Site_AttractionController.java
+++ TeamProject/src/main/java/com/company/controller/Site_AttractionController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.company.admin.command.AdminCriteria;
 import com.company.admin.command.AdminPageVO;
